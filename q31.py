dc573cd69ee55b098964c55de82dfdc1fbcd15a2
mwc <mwc@clayto.org>
2010-07-14 16:09:58 +0000

    q31.py is smart!

--- q31.py
+++ q31.py
@@ -13,53 +13,27 @@
 # 
 # How many different ways can L2 be made using any number of coins?
 
-from json import loads
-
-# Right now, this solution is ridiculously stupid and doesn't work
-# At All.
-
 coins = [ 1, 2, 5, 10, 20, 50, 100, 200 ]
-coins.reverse()
 
-ANSWERS = {}
+def ways( n, c ):
 
-def ways( n, c=[] ):
+    "n is the quantity we want to match.  c is the previously chosen coin."
 
-    if n == 0: 
-        c.reverse()
-        #print(int("".join(map( str, c ))))
-        ANSWERS[ str(c) ] = 0
-        c = []
-        return 0
-    if n  < 0: return 0
-    
-    running_sum = 0
+    if n == 0: return 1 # if we hit the mark, count it as 1 solution
+    if n <= 0: return 0 # if we overshot and spent too much, no solution
 
-    for coin in coins:
-        if coin <= n:
-            c.append(coin)
-            running_sum += ways( n - coin, c )
+    # get every coin that is less than or equal to n (the remaining amount
+    # of money), and greater than or equal to c (the last coin we used).
+    # c exists so that we don't wind up with duplicate solutions like 
+    # (1,2,2) and (2,2,1) for 5pence.
+    possibilities = [ coin for coin in coins if coin <= n and coin >= c ]
+    count = 0
 
-    return running_sum
+    for coin in possibilities:
+        count += ways( n - coin, coin )
+
+    return count
 
 if __name__ == "__main__":
     
-    want = 10
-
-    ways(want)
-
-    total = 0
-    running_sum = 0
-    all_combos = loads(ANSWERS.keys()[len(ANSWERS)-1])
-    for n in all_combos:
-        running_sum += n
-        if running_sum == want:
-            running_sum = 0
-            total += 1
-        elif running_sum > want:
-            print("WOAH")
-            break
-
-    print( total )
-        
-
+    print( ways( 200, 0 ) )
